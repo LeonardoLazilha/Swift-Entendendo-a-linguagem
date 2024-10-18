2ef11592d483b073a213245bde6b1c8a5b32716e
Leonardo Lazilha <107946409+LeonardoLazilha@users.noreply.github.com>
2024-10-17 22:12:01 -0300

    loops

--- aula03-controle-de-fluxo-com-loopings/loops.playground/Contents.swift
+++ aula03-controle-de-fluxo-com-loopings/loops.playground/Contents.swift
@@ -1,3 +1,68 @@
-import UIKit
+// FOR
 
-var greeting = "Hello, playground"
+for i in 1...10 {
+    print("Swift")
+}
+
+//--
+for _ in 1...10 {
+    print("Swift")
+}
+
+
+// WHILE
+
+var count = 1
+
+while count <= 10{
+    print("Eu amo a linguagem Swift")
+    count += 1
+}
+
+
+// REPEAT WHILE
+
+repeat {
+    print("Eu amo a linguagem Swift")
+} while count <= 10
+
+
+// Qual a diferença entre while e repeat while?
+
+/* A diferença entre os dois métodos é que o while realiza primeiro a verificação da condição; no caso, count ser menor ou igual a 10. Se o count fosse igual a 11, ele nunca entraria nessa condição */
+
+var soma = 0
+
+for i in 1...10 {
+    soma += i
+}
+print(soma)
+
+
+
+// BREAK - interrompe o loop quando 'conta' for igual a 5
+
+var conta = 1
+
+while conta <= 10 {
+    if conta == 5 {
+            break
+    }
+    print(conta)
+    conta += 1
+}
+
+// CONTINUE - pula a iteração quando 'contador' for igual a 5
+
+var contador = 1
+
+while contador <= 10 {
+    contador += 1
+    if contador == 5 {
+            continue
+    }
+    print(contador)
+}
+
+
+
